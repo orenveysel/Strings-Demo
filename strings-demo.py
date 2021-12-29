743529d83aecc1474dc5ae09c4f132cec589c5d6
Veysel ÖREN <orenveysel@outlook.com>
2021-12-29 21:56:39 +0300

    Create strings-demo.py

---
+++ strings-demo.py
@@ -0,0 +1,25 @@
+website = "http://www.sadikturan.com"
+course  = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+
+# 1-    print(len(course))
+
+# 2-    print(website[7:10])     
+
+# 3-    print(website[-3:])
+
+# 4(a)- print(course[:15])
+# 4(b)- print(course[-15:])
+
+# 5-    print(course[::-1])            
+
+name, surname, age, job = 'Bora', 'Yılmaz', 32, 'mühendis'
+
+# 6- print(f"Benim adım {name} {surname}, Yaşım {age} ve mesleğim {job}")
+
+# 7- x = "Hello world"
+# 7- x = x[0:6] + 'W' + x[7:]
+# 7- print(x)
+
+# 8- result = "abc " * 3
+# 8- print(result)
